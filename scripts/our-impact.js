32c533a40a48a02840f13aaa598f212b9e59502f
Bishop <ampomahmichael321@gmail.com>
2026-03-18 10:31:04 +0000

    added the impact so far, impact in action and field updates section for both mobile and desktop

--- scripts/our-impact.js
+++ scripts/our-impact.js
@@ -9,3 +9,18 @@ const donationStorySwiper = new Swiper(".mobile-impact-in-action-swiper", {
     disableOnInteraction: false, // keeps autoplay running after user swipes
   },
 });
+
+const desktopDonationStorySwiper = new Swiper(
+  ".desktop-impact-in-action-swiper",
+  {
+    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 20,
+    direction: "horizontal",
+    loop: false,
+    autoplay: {
+      delay: 4000, // time between slides (ms) → 4000ms = 4 seconds
+      disableOnInteraction: false, // keeps autoplay running after user swipes
+    },
+  },
+);
