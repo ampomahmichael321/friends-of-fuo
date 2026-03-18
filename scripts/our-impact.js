c73d8b5193408776bc98f725ff8c1d96d330da01
Bishop <ampomahmichael321@gmail.com>
2026-03-18 11:17:22 +0000

    added and styled the plaque section for both desktop and mobile. I also added the swiper in the plaque section

--- scripts/our-impact.js
+++ scripts/our-impact.js
@@ -24,3 +24,15 @@ const desktopDonationStorySwiper = new Swiper(
     },
   },
 );
+
+const plaqueSwiper = new Swiper(".plaque-section-swiper", {
+  // Optional parameters
+  slidesPerView: 1,
+  spaceBetween: 20,
+  direction: "horizontal",
+  loop: false,
+  autoplay: {
+    delay: 4000, // time between slides (ms) → 4000ms = 4 seconds
+    disableOnInteraction: false, // keeps autoplay running after user swipes
+  },
+});
