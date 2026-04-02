b2bd1d8cd9ed8be39c4677d77dacfcb4b6d3cb44
Bishop <ampomahmichael321@gmail.com>
2026-04-02 20:30:18 +0000

    added the two muralists as a slider

--- scripts/our-impact.js
+++ scripts/our-impact.js
@@ -36,3 +36,15 @@ const plaqueSwiper = new Swiper(".plaque-section-swiper", {
     disableOnInteraction: false, // keeps autoplay running after user swipes
   },
 });
+
+const fieldUpdatesSwiper = new Swiper(".field-updates-swiper", {
+  // Optional parameters
+  slidesPerView: 1,
+  spaceBetween: 20,
+  direction: "horizontal",
+  loop: false,
+  autoplay: {
+    delay: 4000, // time between slides (ms) → 4000ms = 4 seconds
+    disableOnInteraction: false, // keeps autoplay running after user swipes
+  },
+});
